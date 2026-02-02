a14fd2e4b9a45003ae435c3cf1e566164122ac29
Popova Vesta <Dooshneelah1337@gmail.com>
2026-02-02 23:08:14 +0700

    update

--- src/components/widgets/SectionCarousel.tsx
+++ src/components/widgets/SectionCarousel.tsx
@@ -9,9 +9,9 @@ interface SlideItem {
 }
 
 const SLIDE_ITEMS: SlideItem[] = [
-  { image: "./energofoodgroup/2.jpeg" },
-  { image: "./energofoodgroup/1.jpeg" },
-  { image: "./energofoodgroup/3.jpeg" }
+  { image: "./2.jpeg" },
+  { image: "./1.jpeg" },
+  { image: "./3.jpeg" }
 ]
 
 export const SectionCarousel = () => {
